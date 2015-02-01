ab48a5e7e2497f718510cb42491c450ccb624451
Alexey Proskuryakov <alexey.proskuryakov@gmail.com>
2015-02-02 00:45:52 +0300

    like wsgi app

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,16 @@
 # coding:utf-8
 __author__ = '4ikist'
+import sys
+
+
+def application(env, start_response):
+    sys.stderr.write('app ')
+    start_response('200 OK', [('Content-Type','text/html')])
+    return ["Hello World"]
+
 if __name__ == '__main__':
     from start import main
     main()
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost',8080,application)
+    srv.serve_forever(3)
