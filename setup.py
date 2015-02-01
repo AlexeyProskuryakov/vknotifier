b316abfde3d644fb75d4456a082d6916e3e1e0b6
Alexey Proskuryakov <alexey.proskuryakov@gmail.com>
2015-02-02 00:47:03 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 __author__ = '4ikist'
 from setuptools import setup
 import os
-packages = ['request==2.2.1','lxml==3.4.0','pymongo==2.6.3']
+packages = ['requests==2.2.1','lxml==3.4.0','pymongo==2.6.3']
 setup(name='vknotifier',          # <= Put your application name, in this case 'mysite'
        version='1.0',
        description='bot for todo or notifications at vk', # <= Put your description if you want
